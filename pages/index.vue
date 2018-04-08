02de1973542db08ed6794bbc6b56f27e73e7edbc
Oscar <o@ovlb.net>
2018-04-08 13:22:46 +0200

    add pages

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <the-content/>
+</template>
+
+<script>
+import TheContent from '~/components/TheContent.vue'
+
+export default {
+  components: {
+    TheContent
+  }
+}
+</script>
+
+<style>
+
+</style>
